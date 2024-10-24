a9f1afd77aa9bc8407b4d3df371f0a832d83a6be
Zahara <zaharaoyen123@gmail.com>
2024-10-24 10:26:14 +0700

    Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -7,26 +7,25 @@ Scanner scanner = new Scanner(System.in);
 System.out.print("Masukkan total penjualan salesman: ");
  double totalPenjualan = scanner.nextDouble();
         
- // Variabel untuk menyimpan komisi
+ 
     double komisi = 0.0;
 
- // Menghitung komisi berdasarkan ketentuan
+ // Menghitung komisi
     if (totalPenjualan <= 500000) {
   komisi = totalPenjualan * 0.10;
  } else {
  // Komisi untuk Rp 500.000 pertama
          komisi = 500000 * 0.10;
-// Menghitung sisa penjualan
+ // Menghitung sisa penjualan
         double sisaPenjualan = totalPenjualan - 500000;
   // Komisi untuk sisa penjualan
          komisi += sisaPenjualan * 0.15;
         }
-
- // Output hasil
-        System.out.printf("Total penjualan salesman: %.0f\n", totalPenjualan);
-        System.out.printf("Komisi yang didapatkan = %.1f\n", komisi);
+  System.out.printf("Total penjualan salesman: %.0f\n", totalPenjualan);
+  System.out.printf("Komisi yang didapatkan = %.1f\n", komisi);
         
-        scanner.close();
+  scanner.close();
+        
     }
 }
 
