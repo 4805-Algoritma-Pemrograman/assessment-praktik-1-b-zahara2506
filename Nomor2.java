db89f88621a8b4fd8a72dfedd9dafc67ad029e5e
Zahara <zaharaoyen123@gmail.com>
2024-10-24 09:59:59 +0700

    Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -3,6 +3,7 @@ import.java.until.Scanner;
 public class Nomor2{
     public static void main(String[] args){
     Scanner scaner = new Scaner (system.in);
+        
     // Tarif per malam dan biaya pelayanan
         double[] tarip = {0, 50000, 40000, 30000};
         double[] biayaPelayananPersen = {0, 0.30, 0.20, 0.10}; 
@@ -25,22 +26,18 @@ public class Nomor2{
             double biayaPelayanan = biayaMenginap * biayaPelayananPersen[kodeBungalow];
             double totalBiaya = biayaMenginap + biayaPelayanan;
             
-            // Update total pendapatan dan jumlah transaksi
             totalPendapatan += totalBiaya;
             jumlahTransaksi++;
             
-            // Tanya pengguna untuk input lagi
             System.out.print("Input lagi (Y/N)?: ");
             char input = scanner.next().charAt(0);
             inputLagi = (input == 'Y' || input == 'y');
         }
-
-        // Output hasil
+        
         System.out.println("Jumlah transaksi penyewaan bungalow = " + jumlahTransaksi);
         System.out.println("Total pendapatan = " + totalPendapatan);
         
         scanner.close();
-      
 
  
     }
